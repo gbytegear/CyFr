a9fb41bc2b16f1e92992a5788a3a9ffb1dacb63e
sh13max <shamanmax13@yandex.ru>
2019-12-12 11:04:52 +0300

     

--- public/src/js/elements/style.js
+++ public/src/js/elements/style.js
@@ -1,27 +1,22 @@
+
+/* IMAGE CONTROLLER */
+
+jscss.add("image-ctrlr", {
+    display: "block",
+    overflow: "hidden",
+    position: "relative",
+    background_size: "cover",
+    background_position: "center",
+    background_color : new Color(0, 0, 0)
+});
+
 jscss.add("image-ctrlr>.image-download", {
     position: "absolute",
     right: "-30px",
     top: "-30px",
     width: "30px",
-    height: "30px",
-    border_radius: "15px",
-    border: ["2px", "solid", "var(--accent-1)"],
-    background_color: "var(--foreground)",
-    transition: '1s'
+    height: "30px"
 });
-
-jscss.add("image-ctrlr>.image-download svg", {
-    fill: "var(--accent-1)",
-    transition: '.5s'
-});
-
-jscss.add("image-ctrlr>.image-download:hover svg", {
-    fill: "var(--accent-2)"
-});
-
-jscss.add("image-ctrlr>.image-download:hover", {
-    border_color: "var(--accent-2)"
-})
 
 jscss.add("image-ctrlr:hover>.image-download", {
     right: "10px",
@@ -36,8 +31,6 @@ jscss.add("image-ctrlr::after", {
     bottom: "-50px",
     left: 0,
 
-    background_color: new Color(0,0,0,128),
-    color: "var(--accent-1)",
     text_align: "center",
     font_size: "2em",
 
@@ -49,25 +42,4 @@ jscss.add("image-ctrlr::after", {
 
 jscss.add("image-ctrlr:hover::after", {
     bottom: 0,
-})
-
-jscss.add("section image-ctrlr", {
-    display: "block",
-    overflow: "hidden",
-    position: "relative",
-    background_size: "cover",
-    background_position: "center",
-    background_color : new Color(0, 0, 0),
-    width: "500px",
-    height: "500px",
-    margin: "auto",
-    border_radius: "20px",
-    margin_bottom: "10px"
 });
-
-jscss.add("section image-ctrlr", {
-    width: "100%",
-    height: "500px",
-    margin: "0",
-    margin_bottom: "10px"
-}, "screen and (max-width: 690px)");
