3254ea04258f4e68d1d21a5374b82bdd8937a8d5
Max <shamanmax13@yandex.ru>
2019-12-16 08:28:25 +0300

    _

---
+++ public/src/js/editor_controller.js
@@ -0,0 +1,20 @@
+try{
+
+const editor = new (class {
+	constructor (){
+		
+	}
+});
+
+const blocks = document.querySelector('.editor-blocks');
+
+const addLineBlock = (props, name)=>{
+	let element = document.createElement('input');
+	element.type = 'text';
+	element.className = name;
+	//elememt.dataset.props = JSON.stringify(props);
+	blocks.appendChild(element);
+};
+
+editor_title_first.addEventListener('click', ()=>addLineBlock({},'h1'));
+}catch(e){alert(e);}
