f57c2fcdec433e8bd8dbc8917d12d23874cdcf71
Max <shamanmax13@ya.ru>
2019-12-19 16:09:01 +0300

    Rewrite design

--- public/src/js/elements/slider.js
+++
@@ -1,3 +0,0 @@
-const Slider = class Slider extends HTMLElement {
-    constructor (){}
-};
---
+++ public/src/js/modules/resizer.js
@@ -0,0 +1,51 @@
+const resizeNavigationBar = (e) => {
+    if(e.clientX > document.documentElement.clientWidth - 100) 
+        return document.documentElement.style
+        .setProperty('--nav', `calc(100% - 16px)`);;
+    document.documentElement.style
+    .setProperty('--nav', `${e.clientX/(document.documentElement.clientWidth/100)}%`);
+    document.documentElement.style
+        .setProperty('--show-nav', `1`);
+    if(e.clientX < 150){
+        document.documentElement.style
+        .setProperty('--nav', `0px`);
+        document.documentElement.style
+        .setProperty('--show-nav', `0`);
+    }
+}
+
+const resizeNavigationBarTouch = (e) => {
+    resizeNavigationBar({clientX: e.changedTouches[0].screenX, clientY: e.changedTouches[0].screenY});
+}
+
+resizer.addEventListener('mousedown',()=>{
+    document.documentElement.addEventListener('mousemove', resizeNavigationBar);
+});
+
+resizer.addEventListener('touchstart',()=>{
+    document.documentElement.addEventListener('touchmove', resizeNavigationBarTouch);
+});
+
+document.addEventListener('mouseup',()=>{
+    document.documentElement.removeEventListener('mousemove', resizeNavigationBar);
+});
+
+resizer.addEventListener('touchend',()=>{
+    document.documentElement.removeEventListener('touchmove', resizeNavigationBarTouch);
+});
+
+
+resizer.addEventListener('dblclick', ()=> {
+    document.documentElement.style
+    .setProperty('--nav', null);
+    document.documentElement.style
+    .setProperty('--show-nav', null);
+});
+
+open_menu.addEventListener('click', ()=>{
+    document.documentElement.classList.add('open-menu');
+});
+
+close_menu.addEventListener('click', ()=>{
+    document.documentElement.classList.remove('open-menu');
+});
